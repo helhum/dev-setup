b32edc1a891fe168304a3cc847c5dd71f3fce78f
Helmut Hummel <typo3@helhum.io>
2022-04-28 21:38:46 +0200

    Remove setting up zsh

--- .brew.sh
+++ .brew.sh
@@ -15,7 +15,6 @@ if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Make sure we’re using the latest Homebrew.
