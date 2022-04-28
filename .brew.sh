f02b3dd18db08b2b7cb90b33cf52e6c6948459ed
Helmut Hummel <typo3@helhum.io>
2022-04-28 19:11:50 +0200

    Fix brew path

--- .brew.sh
+++ .brew.sh
@@ -15,7 +15,8 @@ if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-alias brew='/opt/homebrew/bin/brew'
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Make sure we’re using the latest Homebrew.
 brew update
