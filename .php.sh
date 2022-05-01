4aef2b8992a237e6001d2c6fd35a71ea94181e49
Helmut Hummel <typo3@helhum.io>
2022-05-01 02:04:55 +0200

    Finish PHP setup script

--- .php.sh
+++ .php.sh
@@ -13,15 +13,12 @@ binaries=(
 
 brew install ${binaries[@]}
 
-
-exit 0
-
 # Install composer
 if [ -f ~/.bin/composer ]; then
   composer self-update
 else
   php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-  php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  php -r "if (hash_file('SHA384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
   php composer-setup.php --install-dir=$HOME/.bin --filename=composer
   php -r "unlink('composer-setup.php');"
 fi
@@ -30,14 +27,7 @@ fi
 composerPackages=(
   bamarni/symfony-console-autocomplete
   friendsofphp/php-cs-fixer
+  helhum/typo3-deployer-recipe:dev-main
 )
 
-php $HOME/.bin/composer global require ${composerPackages[@]}
-
-# Link MAMP PHP binaries to local path
-rm ~/.bin/php*
-for phpVersion in /Applications/MAMP/bin/php/php*
-do
-  ln -snvf $phpVersion/bin/php "$HOME/.bin/$(basename $phpVersion | sed -e 's/\(php[^.]*\)\.\([^.]*\)\(.*\)/\1\2/g')"
-done
-ln -snvf $phpVersion/bin/php "$HOME/.bin/php"
+php74 $HOME/.bin/composer global require ${composerPackages[@]}
